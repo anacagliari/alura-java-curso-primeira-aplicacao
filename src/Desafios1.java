442cb7c4e8b1b7a0cb295f3ed29b185473171764
Ana Cagliari <carolinecagliari@gmail.com>
2024-07-17 18:13:16 -0300

    1. Primeiro programa em Java + Desafios

---
+++ src/Desafios1.java
@@ -0,0 +1,15 @@
+public class Desafios1 {
+    public static void main(String[] args) {
+        String nome = "Ana Caroline";
+        System.out.println("Eu, " + nome + ", concluí a aula 01 e agora estou mergulhando em Java!");
+
+        int x = 10;
+        int y = 5;
+
+        int soma = x + y;
+        System.out.println("A soma entre " + x + " e " + y + " é igual a " + soma);
+
+        int subtracao = x - y;
+        System.out.println("A subtração entre " + x + " e " + y + " é igual a " + subtracao);
+    }
+}
