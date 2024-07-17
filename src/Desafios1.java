ce354d21b18ccf38b5270052eea0c64ba8700d5c
Ana Cagliari <carolinecagliari@gmail.com>
2024-07-17 18:14:34 -0300

    2. Construindo o catálogo de filmes

--- src/Desafios1.java
+++ src/Desafios1.java
@@ -11,5 +11,30 @@ public class Desafios1 {
 
         int subtracao = x - y;
         System.out.println("A subtração entre " + x + " e " + y + " é igual a " + subtracao);
+
+        double temperaturaCelsius = 16.5;
+        double converteTemperatura = (temperaturaCelsius * 1.8) + 32;
+        System.out.println("A temperatura em graus Celsius " + temperaturaCelsius + "°C convertida para grau Fahrenheit é de " + converteTemperatura + "°F");
+
+        int media = (int) (temperaturaCelsius + converteTemperatura) / 2;
+        System.out.println("A média é: " + media);
+
+        char letra = 'D';
+        String palavra = "esafio";
+        System.out.println(letra + palavra);
+
+        double precoProduto = 14.99;
+        int quantidade = 7;
+        System.out.println("Valor total: RS " + (precoProduto * quantidade));
+
+        double valorEmDolar = 234.82;
+        double cotacaoEmReal = 5.495; // dia 17.07.2024
+        double conversaoParaReal = valorEmDolar * cotacaoEmReal;
+        System.out.println(String.format("Valor convertido de US$ %.2f para R$ %.2f", valorEmDolar, conversaoParaReal));
+
+        double precoOriginal = 29.80;
+        double percentualDesconto = precoOriginal * 0.1;
+        double valorTotalDesconto = precoOriginal - percentualDesconto;
+        System.out.println("Valor com desconto: R$ " + valorTotalDesconto);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao Screen Match!");
+
+        int anoLancamento = 2002;
+        String nomeFilme = "Harry Potter e a Pedra Filosofal";
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.6;
+        double mediaNotas = (notaDoFilme + 7.9 + 9.4) / 3;
+        String sinopse = """
+                Filme de bruxos crianças em Hogwarts.
+                """;
+        int classificacao = (int) (mediaNotas / 2); // casting explícito
+
+        System.out.println("Filme : " + nomeFilme);
+        System.out.println("Ano de Lançamento: " + anoLancamento);
+        System.out.println("Incluido no plano? " + incluidoNoPlano);
+        System.out.println(String.format("Média da Nota: %.2f", mediaNotas));
+        System.out.println("Sinopse: " + sinopse);
+        System.out.println(String.format("O nome do filme é %s, o ano de lançamento é %d e a classificação é %d.", nomeFilme, anoLancamento, classificacao));
     }
 }
